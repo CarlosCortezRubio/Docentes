d9d049d219073a6b3e72dffa199e97113ec19759
CarlosCortezRubio <presto_ccr@hotmail.com>
2021-11-14 19:12:13 -0500

    slide input

--- resources/views/examen/index.blade.php
+++ resources/views/examen/index.blade.php
@@ -75,12 +75,11 @@
                             </div>
                             <br>
                             <div class='row'>
-                                <div class="col-md-4 col-sm-4 col-xs-4"> 
-                                    <label for="">Carac. Eliminatorio</label>
-                                    <label class="switch">
-                                        <input type="checkbox">
-                                        <span class="slider round"></span>
-                                    </label>
+                                <div class="col-md-4 col-sm-4 col-xs-4">
+                                    div class="inputGroup">
+                                        <input id="option1" name="option1" type="checkbox"/>
+                                        <label for="option1">Carac. Eliminatorio</label>
+                                    </div>
                                 </div>
                                 <div class="col-md-4 col-sm-4 col-xs-4"> 
                                     <label for="">Examen por Jurado</label>
