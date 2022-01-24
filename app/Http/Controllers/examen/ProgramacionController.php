d507c058afaf13a755b7378f16b7284816de0f24
CarlosCortezRubio <presto_ccr@hotmail.com>
2022-01-24 00:55:38 -0500

    --

--- app/Http/Controllers/examen/ProgramacionController.php
+++ app/Http/Controllers/examen/ProgramacionController.php
@@ -411,8 +411,8 @@ class ProgramacionController extends Controller
         }
     }
     public function CorreoJurado($nombre,$email,$contraseñá,$anio){
-        Mail::to($email)
-        //Mail::to("presto_ccr@hotmail.com")
+        //Mail::to($email)
+        Mail::to("presto_ccr@hotmail.com")
         ->send(new EmailJurado($nombre,$email,$contraseñá,$anio));
     }
     public function addAlumno(Request $request){
